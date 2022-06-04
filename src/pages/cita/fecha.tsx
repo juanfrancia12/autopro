2f693ecf7a128c3690870f5e65f22e77b1813935
Juan Francia <jf2381886@gmail.com>
2022-06-03 21:42:24 -0500

    estructura inicial

---
+++ src/pages/cita/fecha.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DatePage = () => {
+  return <div></div>
+}
+
+export default DatePage
---
+++ src/pages/cita/reservar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ReservePage = () => {
+  return <div></div>
+}
+
+export default ReservePage
